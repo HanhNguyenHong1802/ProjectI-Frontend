b35d444968309fbe13b1e1f49df693005062a003
hanh2112 <hanhnguyee2000@gmail.com>
2023-07-19 23:56:56 +0700

    Fix home UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,65 @@
 import React from 'react';
-import { Row, Card, CardImg, CardBody, CardTitle} from 'reactstrap';
+import { Row, Card, CardImg, CardBody, CardTitle } from 'reactstrap';
 import Image from 'react-bootstrap/Image';
 import { baseUrl } from '../shared/baseUrl';
 import { Link } from 'react-router-dom';
-
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 function Home(props) {
 
-        return  (
-            <>
+  return (
+    <>
+      <div>
+        <Row className=" justify-content-center" >
+          {/* <Image className="col-10 col-md-10 m-1" src= {baseUrl+"images/gacuon.jpg"} fluid/> */}
+          <Carousel>
             <div>
-	            <Row className=" justify-content-center" >
-	                <Image className="col-10 col-md-10 m-1" src= {baseUrl+"images/logo.jpg"} fluid/>
-	            </Row>
-	            <Row className="m-1 justify-content-center" >	                
-		            <div className="col-12 col-md m-1">
-		            <Link to="/menu">
-		            <Card>
-	                    <CardImg src={baseUrl+"images/dine_in.jpg"} className ="card-img-top"/>
-	                    <CardBody>
-	                        <CardTitle>{"Dine In"}</CardTitle>
-	                    </CardBody>
-	                </Card>
-	                </Link>
-	                </div>
-	                <div className="col-12 col-md m-1">
-	                <Link to="/menu">
-		            <Card>
-	                    <CardImg src={baseUrl +"images/take_out.jpg"} className ="card-img-top"/>
-	                    <CardBody>
-	                        <CardTitle>{"Take Out"}</CardTitle>
-	                    </CardBody>
-	                </Card>
-	                </Link>
-	                </div>
-	                <div className="col-12 col-md m-1">
-	                <Link to="/menu">
-		            <Card>
-	                    <CardImg src={baseUrl +"images/monthly_special.jpg"} className ="card-img-top"/>
-	                    <CardBody>
-	                        <CardTitle>{"Recommended"}</CardTitle>
-	                    </CardBody>
-	                </Card>
-	                </Link>
-	                </div>
-	            </Row>
+              <Image className="col-10 col-md-10 m-1" src= {baseUrl+"images/gacuon.jpg"} />
             </div>
+            <div>
+              <Image className="col-10 col-md-10 m-1" src= {baseUrl+"images/buatruavuive.webp"} />
+            </div>
+            <div>
+              <Image className="col-10 col-md-10 m-1" src= {baseUrl+"images/Homde-phase2.webp"} />
+            </div>
+          </Carousel>
+        </Row>
+        <Row className="mx-5 justify-content-center" >
+          <div className="col-12 col-md m-1">
+            <Link to="/menu">
+              <Card>
+                <CardImg src={baseUrl + "images/GA.jpg"} className="card-img-top" />
+                <CardBody>
+                  <CardTitle>{"Dine In"}</CardTitle>
+                </CardBody>
+              </Card>
+            </Link>
+          </div>
+          <div className="col-12 col-md m-1">
+            <Link to="/menu">
+              <Card>
+                <CardImg src={baseUrl + "images/combo_1_nguoi.jpg"} className="card-img-top" />
+                <CardBody>
+                  <CardTitle>{"Take Out"}</CardTitle>
+                </CardBody>
+              </Card>
+            </Link>
+          </div>
+          <div className="col-12 col-md m-1">
+            <Link to="/menu">
+              <Card>
+                <CardImg src={baseUrl + "images/COM.jpg"} className="card-img-top" />
+                <CardBody>
+                  <CardTitle>{"Recommended"}</CardTitle>
+                </CardBody>
+              </Card>
+            </Link>
+          </div>
+        </Row>
+      </div>
 
-            </>
-      );
+    </>
+  );
 
 }
 
